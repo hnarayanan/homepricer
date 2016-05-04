d0fff64bc13d71a9f2037d887dcf98dddd982ad9
Harish Narayanan <hnarayanan@gmail.com>
2016-05-04 21:46:36 +0100

    Add initial models to hold house pricing data

--- property_prices/models.py
+++ property_prices/models.py
@@ -2,4 +2,50 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Property(models.Model):
+
+    TYPES = (
+        ('D', 'Detached'),
+        ('S','Semi-Detached'),
+        ('T', 'Terraced'),
+        ('F', 'Flats/Maisonettes'),
+        ('O', 'Other'),
+    )
+
+    AGES = (
+        ('Y', 'Newly built'),
+        ('N', 'Established building'),
+    )
+
+    DURATIONS = (
+        ('F', 'Freehold'),
+        ('L', 'Leasehold'),
+    )
+
+    paon = models.CharField(max_length=50, null=True)
+    saon = models.CharField(max_length=50, null=True)
+    street = models.CharField(max_length=50, null=True)
+    locality = models.CharField(max_length=50, null=True)
+    postcode = models.CharField(max_length=10, null=True)
+    town_or_city = models.CharField(max_length=50, null=True)
+    district = models.CharField(max_length=50, null=True)
+    county = models.CharField(max_length=50, null=True)
+
+    type = models.CharField(max_length=1, choices=KINDS)
+    age = models.CharField(max_length=1, choices=AGES)
+    duration = models.CharField(max_length=1, choices=DURATIONS)
+
+
+class Transaction(models.Model):
+
+    CATEGORIES = (
+        ('A', 'Standard Price Paid'),
+        ('B', ' Additional Price Paid'),
+    )
+
+    id = models.UUIDField(primary_key=True)
+    property = Models.ForeignKey(Property)
+    price = models.DecimalField(max_digits=9)
+    category = models.CharField(max_length=1, choices=CATEGORIES)
+    transfer_date = models.DateField()
