1df3ce9aff855c53e905c5263ee85b5952671d8a
Harish Narayanan <hnarayanan@gmail.com>
2016-05-05 09:04:53 +0100

    Expand the admin list displays of the two models

--- property_prices/admin.py
+++ property_prices/admin.py
@@ -3,5 +3,25 @@ from django.contrib import admin
 from .models import Property, Transaction
 
 
-admin.site.register(Property)
-admin.site.register(Transaction)
+class PropertyAdmin(admin.ModelAdmin):
+
+    list_display = ('__unicode__', 'type', 'age', 'duration',)
+    list_filter = ('type', 'age', 'duration',)
+    search_fields = ['paon', 'saon', 'street', 'locality', 'postcode',
+                    'town_or_city', 'district', 'county']
+
+
+class TransactionAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'property', 'price', 'category',
+                    'transfer_date',)
+    list_filter = ('category', 'transfer_date',)
+    search_fields = ['id']
+
+    # TODO: Fields that aren't either searchable or filter-able
+    # property = models.ForeignKey(Property, related_name='transactions', null=True)
+    # price = models.DecimalField(max_digits=9, decimal_places=0, null=True)
+
+
+admin.site.register(Property, PropertyAdmin)
+admin.site.register(Transaction, TransactionAdmin)
