a236f91e745c4f15db684f22358927707178e70f
Harish Narayanan <hnarayanan@gmail.com>
2017-04-20 21:07:38 +0100

    Update styling of the colours

--- visualisation/scripts/app.js
+++ visualisation/scripts/app.js
@@ -3,16 +3,16 @@ function getColor() {
     // set. It needs to return the actual average property price in
     // the enclosing shape.
     var colors = ['#800026', '#BD0026', '#E31A1C', '#FC4E2A', '#FD8D3C', '#FEB24C', '#FED976', '#FFEDA0'];
+    // var colors = ['#0c2c84', '#225ea8', '#1d91c0', '#41b6c4', '#7fcdbb', '#c7e9b4', '#edf8b1', '#ffffd9'];
     return colors[Math.floor(Math.random() * colors.length)];
 }
 
 function style(feature) {
     return {
         fillColor: getColor(),
-        weight: 2,
-        opacity: 1,
-        color: 'white',
-        dashArray: '3',
+        weight: 0.5,
+        opacity: 0.5,
+        color: '#800026',
         fillOpacity: 0.7
     };
 }
@@ -23,4 +23,4 @@ L.tileLayer(
 	accessToken: 'pk.eyJ1IjoiaG5hcmF5YW5hbiIsImEiOiJjajFwaDF2enEwMDAxMnduejFzZjg1NnEyIn0.KlcHvt6sAtxBrVUflJLTpA'
     }
 ).addTo(map);
-L.geoJson(wards, {style: style}).addTo(map);
+L.geoJson(regions, {style: style}).addTo(map);
