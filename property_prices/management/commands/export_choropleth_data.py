026ae1d375ee259294e861e1bb41824d0cb6d023
Harish Narayanan <hnarayanan@gmail.com>
2017-04-21 21:41:12 +0100

    Complete first pass of the data exporter to generate data for choropleth maps

--- property_prices/management/commands/export_choropleth_data.py
+++ property_prices/management/commands/export_choropleth_data.py
@@ -1,4 +1,8 @@
+import pandas
+import json
+
 from django.core.management.base import BaseCommand
+from django.db.models.aggregates import Count, Avg, Max, Min
 
 from property_prices.models import Transaction
 
@@ -12,32 +16,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         years = options['years']
+        postcode_locations = pandas.read_csv('data/NSPL_FEB_2017_UK.csv').set_index('pcds')
+        wards = postcode_locations.ward.unique()
+        data = {}
         for year in years:
-            transactions = Transaction.objects.filter(transfer_date__year=year).count()
-            self.stdout.write(self.style.SUCCESS('{transactions} transactions in {year}'.format(transactions=transactions, year=year)))
-
-
-
-
-
-# ----------------------
-# NOTE: YEARLY TRANSACTIONS
-# from property_prices.models import Property, Transaction
-
-# for year in range(1995, 2016 + 1):
-#     print year, ",",
-#     for property_type in Property.TYPES:
-#         print Transaction.objects.filter(transfer_date__year=year,
-#                                          property__type=property_type[0]).count(), ",",
-#     print
-
-
-        # Export postcode geocodes
-                # postcode_locations = pandas.read_csv('NSPL_FEB_2016_UK.csv').set_index('pcds')
-        # with open('NW-postcodes.csv', 'w') as f:
-        #     for row in postcode_locations.iterrows():
-        #         if 'NW' in row[0]:
-        #             f.write("new google.maps.LatLng({latitude}, {longitude}),\n".format(
-        #                 latitude=row[1]['lat'],
-        #                 longitude=row[1]['long']
-        #             ))
+            for ward in wards[0:200]:
+#                aggregate = Transaction.objects.filter(property__ward=ward, transfer_date__year=year).aggregate(Count('price'), Avg('price'), Max('price'), Min('price'))
+                average_price = Transaction.objects.filter(property__ward=ward, transfer_date__year=year).aggregate(Avg('price'))['price__avg']
+                data[ward] = average_price
+        print json.dumps(data)
