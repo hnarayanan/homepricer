6d591464f00a447391992a003a7dfe86bac0d36b
Harish Narayanan <hnarayanan@gmail.com>
2017-04-21 21:10:53 +0100

    Start rewriting the data exporter to generate data for choropleth maps

--- property_prices/management/commands/export_choropleth_data.py
+++ property_prices/management/commands/export_choropleth_data.py
@@ -1,35 +1,23 @@
-import numpy
-import pandas
-
 from django.core.management.base import BaseCommand
+
+from property_prices.models import Transaction
+
 
 class Command(BaseCommand):
 
-    help = "Export the coordinates and weights for the heatmap"
+    help = "Export the average prices over time for rendering a choropleth"
+
+    def add_arguments(self, parser):
+        parser.add_argument('years', nargs='+', type=str)
 
     def handle(self, *args, **options):
+        years = options['years']
+        for year in years:
+            transactions = Transaction.objects.filter(transfer_date__year=year).count()
+            self.stdout.write(self.style.SUCCESS('{transactions} transactions in {year}'.format(transactions=transactions, year=year)))
 
-        # Geobounding box of England
-        # top = 1.76896
-        # bottom = -6.37988
-        # left = 49.871159
-        # right = 55.811741
 
-        # Geobounding box of Greater London
-        top = 0.3340155
-        bottom = -0.5103751
-        left = 51.2867602
-        right = 51.6918741
 
-        # Mesh resolution
-        res = 100
-
-        latitudes = numpy.linspace(left, right, res)
-        longitudes = numpy.linspace(bottom, top, res)
-
-        for latitude in latitudes:
-            for longitude in longitudes:
-                print "{location: new google.maps.LatLng(%s, %s), weight: %d}," % (latitude, longitude, numpy.random.random_integers(1, 100))
 
 
 # ----------------------
