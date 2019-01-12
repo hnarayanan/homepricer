ec4ee48af3d06cc2c26260b71870b469a3e8c146
Harish Narayanan <hnarayanan@gmail.com>
2019-01-12 22:59:30 +0000

    Add a simple password for the new local DB

--- homepricer/settings.py
+++ homepricer/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'homepricer',
         'USER': 'harish',
-        'PASSWORD': '',
+        'PASSWORD': 'harish',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
